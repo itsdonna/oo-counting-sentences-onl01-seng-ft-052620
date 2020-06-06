6a9b26a7074308e7333135f12e5c7e3d84e8188f
Donna Torres <todonnatorres@icloud.com>
2020-06-06 09:10:41 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,7 +18,7 @@ class String
 
   def count_sentences
     
-    sentences = self.gsub(".", "142").gsub("?", "142").gsub("!", "142").split("142").reject{|sentence| sentence = ""}
+    sentences = self.gsub(".", "142").gsub("?", "142").gsub("!", "142").split("142").reject {|sentence| sentence = ""}
     sentences.count
     
     # binding.pry
